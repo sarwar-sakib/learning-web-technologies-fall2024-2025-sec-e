9e0ba896357839c839089b42b1d581d2e7ef8938
Sakib Sarwar <152203195+sarwar-sakib@users.noreply.github.com>
2024-11-11 13:56:23 +0600

    Update task7.php

--- lab3/task7.php
+++ lab3/task7.php
@@ -11,7 +11,17 @@ for ($i=0; $i<=2; $i++){
 for ($i=3; $i>=0; $i--){
     for ($j=0; $j<=$i; $j++){
         
-        echo "$i ";
+        echo "$j ";
+    }
+    echo "<br>";
+}
+
+$char = 'A';
+for ($i=0; $i<=2; $i++){
+    for ($j=0; $j<=$i; $j++){
+        
+        echo "$char ";
+        $char++;
     }
     echo "<br>";
 }
